7a55a31d0ed767913a916110cc5cdc9ea0bdbd8b
e-sushi <officialaxos7@gmail.com>
2022-01-06 20:22:50 -0500

    rewrite lexer

--- src/su-lexer.cpp
+++ src/su-lexer.cpp
@@ -1,5 +1,6 @@
 #include "su-lexer.h"
 #include "utils/defines.h"
+#include "utils/cstring.h"
 
 #define MAXBUFF 255
 
@@ -14,168 +15,146 @@ array<string> keywords{
 	"int", "return", "if", "else"
 };
 
+enum LexerState_ {
+	DiscernChar,
+	ReadingString,
+
+}; typedef u32 LexerState;
+
+LexerState state = DiscernChar;
+
 array<token> suLexer::lex(FILE* file) {
 	array<token> tokens;
 	char currChar = 0;
-	string buff = "";
+	char* buffptr = 0;
 	u32 lines = 1;
+
+	fseek(file, 0, SEEK_END);
+	u32 size = ftell(file);
+	buffptr = (char*)malloc(size);
 	
-	//TODO get rid of maxbuff here
-	while (buff.size + 1 < MAXBUFF) {
-		currChar = fgetc(file);
-		//check that our current character isn't any 'stopping' characters
-		if (stopping_chars.has(currChar)) {
+	rewind(file);
+	u32 read = fread(buffptr, 1, size, file);
+	
+	buffptr[read] = 0;
 
-			//store both the stopping character and previous buffer as tokens
-			//also decide what type of token it is
-			if (buff[0]) {
-				token t;
-				t.str = buff;
-				//check if token is a keyword
-				if (keywords.has(buff)) {
-					if      (buff == "return") t.type = Token_Return;
-					else if (buff == "int")    t.type = Token_Keyword;
-					else if (buff == "if")     t.type = Token_If;
-					else if (buff == "else")   t.type = Token_Else;
-				}
-				//if its not then it could be a number of other things
-				else {
-					//TODO make this cleaner
-					if (isalpha(buff[0])) {
-						//if it begins with a letter it must be an identifier
-						//for now
-						t.type = Token_Identifier;
-					}
-					else if (isdigit(buff[0])) {
-						//check if its a digit, then verify that the rest are digits
-						bool error = false;
-						for (int i = 0; i < buff.size; i++) {
-							if (!isdigit(buff[i])) error = true;
-						}
-						if (error) t.type = Token_ERROR;
-						else t.type = Token_Literal;
-					}
-				}
-				t.line = lines;
-				tokens.add(t);
-			}
+	cstring buff{ buffptr, read };
 
-			//check what our stopping character is 
-			if (currChar != ' ' && currChar != '\n') {
-				token t;
-				t.str = currChar;
-				switch (currChar) {
-					case ';':  t.type = Token_Semicolon;         break;
-					case '{':  t.type = Token_OpenBrace;         break;
-					case '}':  t.type = Token_CloseBrace;        break;
-					case '\(': t.type = Token_OpenParen;         break;
-					case '\)': t.type = Token_CloseParen;        break;
-					case ',':  t.type = Token_Comma;             break;
-					case '+':  t.type = Token_Plus;              break;
-					case '-':  t.type = Token_Negation;          break;
-					case '*':  t.type = Token_Multiplication;    break;
-					case '/':  t.type = Token_Division;          break;
-					case '~':  t.type = Token_BitwiseComplement; break;
-					case '%':  t.type = Token_Modulo;            break;
-					case '^':  t.type = Token_BitXOR;            break;
-					case '?':  t.type = Token_QuestionMark;      break;
-					case ':':  t.type = Token_Colon;             break;
-					
+	char* chunk_start = 0;
+#define chunksiz buff.str - chunk_start
+#define chunkcmp chunk_start, chunksiz
+#define chunkstr string(chunk_start, buff.str - chunk_start)
+
+	while (buff.count) {
+		switch (state) {
+			case DiscernChar: {
+				switch (buff[0]) {
+					case ';': { tokens.add(token{ ";", Token_Semicolon,  lines }); advance(&buff); }break;
+					case '{': { tokens.add(token{ "{", Token_OpenBrace,  lines }); advance(&buff); }break;
+					case '}': { tokens.add(token{ "}", Token_CloseBrace, lines }); advance(&buff); }break;
+					case '(': { tokens.add(token{ "(", Token_OpenParen,  lines }); advance(&buff); }break;
+					case ')': { tokens.add(token{ ")", Token_CloseParen, lines }); advance(&buff); }break;
+					case ',': { tokens.add(token{ ",", Token_Comma,      lines }); advance(&buff); }break;
+					case '+': {
+						if (buff[1] == '=') { tokens.add(token{ "+=", Token_PlusAssignment, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "+", Token_Plus, lines }); advance(&buff); }
+					}break;
+					case '-': {
+						if (buff[1] == '=') { tokens.add(token{ "-=", Token_NegationAssignment, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "-", Token_Negation, lines }); advance(&buff); }
+					}break;
+					case '*': {
+						if (buff[1] == '=') { tokens.add(token{ "*=", Token_MultiplicationAssignment, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "*", Token_Multiplication, lines }); advance(&buff); }
+					}break;
+					case '/': {
+						if (buff[1] == '=') { tokens.add(token{ "/=", Token_DivisionAssignment, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "/", Token_Division, lines }); advance(&buff); }
+					}break;
+					case '~': {
+						if (buff[1] == '=') { tokens.add(token{ "~=", Token_BitNOTAssignment, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "~", Token_BitNOT, lines }); advance(&buff); }
+					}break;
 					case '&': {
-						if (fgetc(file) == '&') {
-							t.type = Token_AND;
-							t.str += '&';
-						}
-						else {
-							fseek(file, -1, SEEK_CUR);
-							t.type = Token_BitAND;
-						}
+						if (buff[1] == '=') { tokens.add(token{ "&=", Token_BitANDAssignment, lines }); advance(&buff, 2); }
+						else if (buff[1] == '&') { tokens.add(token{ "&&", Token_AND, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "&", Token_BitAND, lines }); advance(&buff); }
 					}break;
-
 					case '|': {
-						if (fgetc(file) == '|') {
-							t.type = Token_OR;
-							t.str += '|';
-						}
-						else {
-							fseek(file, -1, SEEK_CUR);
-							t.type = Token_BitOR;
-						}
+						if (buff[1] == '=') { tokens.add(token{ "|=", Token_BitORAssignment, lines }); advance(&buff, 2); }
+						else if (buff[1] == '|') { tokens.add(token{ "||", Token_OR, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "|", Token_BitOR, lines }); advance(&buff); }
 					}break;
-
-					case '!': {
-						if (fgetc(file) == '=') {
-							t.type = Token_NotEqual;
-							t.str += '=';
-						}
-						else {
-							fseek(file, -1, SEEK_CUR);
-							t.type = Token_LogicalNOT;
-						}
+					case '^': {
+						if (buff[1] == '=') { tokens.add(token{ "^=", Token_BitXORAssignment, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "^", Token_BitXOR, lines }); advance(&buff); }
 					}break;
-
+					case '%': {
+						if (buff[1] == '=') { tokens.add(token{ "%=", Token_Equal, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "%", Token_Modulo, lines }); advance(&buff); }
+					}break;
 					case '=': {
-						if (fgetc(file) == '=') {
-							t.type = Token_Equal;
-							t.str += '=';
-						}
-						else {
-							fseek(file, -1, SEEK_CUR);
-							t.type = Token_Assignment;
-						}
+						if (buff[1] == '=') { tokens.add(token{ "==", Token_Equal, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "=", Token_Assignment, lines }); advance(&buff); }
 					}break;
-
-					case '>': {
-						char c = fgetc(file);
-						if (c == '=') {
-							t.type = Token_GreaterThanOrEqual; 
-							t.str += '=';
-						}
-						else if (c == '>') {
-							t.type = Token_BitShiftRight;
-							t.str += '>';
-						}
-						else {
-							fseek(file, -1, SEEK_CUR);
-							t.type = Token_GreaterThan;
-						}
+					case '!': {
+						if (buff[1] == '=') { tokens.add(token{ "!=", Token_NotEqual, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "!", Token_LogicalNOT, lines }); advance(&buff); }
 					}break;
-
 					case '<': {
-						char c = fgetc(file);
-						if (c == '=') {
-							t.type = Token_LessThanOrEqual;
-							t.str += '=';
-						}
-						else if (c == '<') {
-							t.type = Token_BitShiftLeft;
-							t.str += '<';
-						}
-						else {
-							fseek(file, -1, SEEK_CUR);
-							t.type = Token_LessThan;
-						}
+						if (buff[1] == '=') { tokens.add(token{ "<=", Token_LessThanOrEqual, lines }); advance(&buff, 2); }
+						tokens.add(token{ "<", Token_LessThan, lines }); advance(&buff);
+					}break;
+					case '>': {
+						if (buff[1] == '=') { tokens.add(token{ ">=", Token_GreaterThanOrEqual, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ ">", Token_GreaterThan, lines }); advance(&buff); }
+					}break;
+					case '?': { tokens.add(token{ "", Token_QuestionMark, lines }); advance(&buff); }break;
+					case ':': { tokens.add(token{ "", Token_Colon, lines }); advance(&buff); }break;
+					case '\n': { lines++; advance(&buff); }break;
+					case '\t':
+					case ' ': { advance(&buff); }break;
+					default: { //if this char is none of the above cases we start looking for a worded token
+						state = ReadingString;
+						chunk_start = buff.str;
+						advance(&buff);
 					}break;
 				}
-				t.line = lines;
-				tokens.add(t);
-			}
-			buff.clear();
-		}
-		else if (currChar == EOF) { 
-			token t;
-			t.str = "End of File";
-			t.type = Token_EOF;
-			t.line = lines;
-			tokens.add(t);
-			break;
-		}
-		else if (currChar != '\t' && currChar != '\n') {
-			//if not then keep adding to buffer if character is not a newline or tab
-			buff += currChar;
-		}
-		if (currChar == '\n') lines++;
-	}
+			}break;
+				
+			case ReadingString: {
+				switch (buff[0]) {
+					case ';': case ' ': case '{': case '}': case '(': case ')':
+					case ',': case '+': case '*': case '/': case '-': case '<': 
+					case '=': case '!': case '~': case '&': case '|': case '\n':
+					case '%': case ':': case '?': case '>': case '^': case '\t': {
+						if (isalpha(*chunk_start)) {
+							if      (!strncmp("return", chunk_start, 6)) tokens.add(token{ "return", Token_Return,     lines });
+							else if (!strncmp("s32",    chunk_start, 3)) tokens.add(token{ "s32",    Token_Signed32,   lines });
+							else if (!strncmp("s64",    chunk_start, 3)) tokens.add(token{ "s64",    Token_Signed64,   lines });
+							else if (!strncmp("u32",    chunk_start, 3)) tokens.add(token{ "u32",    Token_Unsigned32, lines });
+							else if (!strncmp("u64",    chunk_start, 3)) tokens.add(token{ "u64",    Token_Unsigned64, lines });
+							else if (!strncmp("f32",    chunk_start, 3)) tokens.add(token{ "f32",    Token_Float32,    lines });
+							else if (!strncmp("f64",    chunk_start, 3)) tokens.add(token{ "f64",    Token_Float64,    lines });
+							else if (!strncmp("if",     chunk_start, 2)) tokens.add(token{ "if",     Token_If,         lines });
+							else if (!strncmp("else",   chunk_start, 4)) tokens.add(token{ "else",   Token_Else,       lines });
+							else tokens.add(token{ chunkstr, Token_Identifier, lines });
+						}
+						else {
+							b32 valid = 1;
+							forI(buff.str - chunk_start) if (!isnumber(chunk_start[i])) valid = 0;
+							if (valid) tokens.add(token{ chunkstr, Token_Literal, lines });
+							else tokens.add(token{ chunkstr, Token_ERROR, lines }); // TODO error out of the program here
+						}
+						
+						if (buff[0] == '\n') lines++;
+						state = DiscernChar;
 
+					}break;
+					default: { advance(&buff); }break;
+				}
+			}break;
+		}
+	}
 	return tokens;
 }
--- src/su-lexer.h
+++ src/su-lexer.h
@@ -8,59 +8,102 @@
 
 
 enum Token_Type {
-    Token_ERROR,              // when something doesnt make sense during lexing
-    Token_EOF,                // end of file
-    Token_Keyword,            // int, float, etc.
-    Token_Identifier,         // function/variable names
-    Token_OpenParen,          // (
-    Token_CloseParen,         // )
-    Token_OpenBrace,          // {
-    Token_CloseBrace,         // }
-    Token_Comma,              // ,
-    Token_Semicolon,          // ;
-    Token_Return,             // return
-    Token_Literal,            // 1, soon to be any literal
-    Token_Signed32,           // s32,
-    Token_Signed64,           // s64, 
-    Token_Unsigned32,         // u32,
-    Token_Unsigned64,         // u64,
-    Token_Float32,            // f32, float
-    Token_Float64,            // f64, double
-    Token_Assignment,         // =
-    Token_Plus,               // +
-    Token_Negation,           // -
-    Token_Multiplication,     // *
-    Token_Division,           // /
-    Token_LogicalNOT,         // !
-    Token_BitwiseComplement,  // ~
-    Token_LessThan,           // <
-    Token_GreaterThan,        // >
-    Token_LessThanOrEqual,    // <=
-    Token_GreaterThanOrEqual, // >=
-    Token_AND,                // &&
-    Token_BitAND,             // &
-    Token_OR,                 // ||
-    Token_BitOR,              // |
-    Token_Equal,              // ==
-    Token_NotEqual,           // !=
-    Token_BitXOR,             // ^
-    Token_BitShiftLeft,       // <<
-    Token_BitShiftRight,      // >>
-    Token_Modulo,             // %
-    Token_QuestionMark,       // ?
-    Token_Colon,              // :
-    Token_If,                 // if
-    Token_Else,               // else
+    Token_ERROR,                    // when something doesnt make sense during lexing
+    Token_EOF,                      // end of file
+    Token_Identifier,               // function/variable names
+    Token_Return,                   // return
+    Token_Literal,                  // 1, 2, 3.221, "string", 'c'
+    Token_Signed32,                 // s32 
+    Token_Signed64,                 // s64 
+    Token_Unsigned32,               // u32 
+    Token_Unsigned64,               // u64 
+    Token_Float32,                  // f32 
+    Token_Float64,                  // f64 
+    Token_Semicolon,                // ;
+    Token_OpenBrace,                // {
+    Token_CloseBrace,               // }
+    Token_OpenParen,                // (
+    Token_CloseParen,               // )
+    Token_Comma,                    // ,
+    Token_Plus,                     // +
+    Token_PlusAssignment,           // +=
+    Token_Negation,                 // -
+    Token_NegationAssignment,       // -=
+    Token_Multiplication,           // *
+    Token_MultiplicationAssignment, // *=
+    Token_Division,                 // /
+    Token_DivisionAssignment,       // /=
+    Token_BitNOT,                   // ~
+    Token_BitNOTAssignment,         // ~=
+    Token_BitAND,                   // &
+    Token_BitANDAssignment,         // &=
+    Token_AND,                      // &&
+    Token_BitOR,                    // |
+    Token_BitORAssignment,          // |=
+    Token_OR,                       // ||
+    Token_BitXOR,                   // ^
+    Token_BitXORAssignment,         // ^=
+    Token_BitShiftLeft,             // <<
+    Token_BitShiftRight,            // >>
+    Token_Modulo,                   // %
+    Token_ModuloAssignment,         // %=
+    Token_Assignment,               // =
+    Token_Equal,                    // ==
+    Token_LogicalNOT,               // !
+    Token_NotEqual,                 // !=
+    Token_LessThan,                 // <
+    Token_LessThanOrEqual,          // <=
+    Token_GreaterThan,              // >
+    Token_GreaterThanOrEqual,       // >=
+    Token_QuestionMark,             // ?
+    Token_Colon,                    // :
+    Token_If,                       // if
+    Token_Else,                     // else
 
 };
 
 static const char* tokenStrings[] = {
-	"ERROR", "EOF", "Keyword", "Identifier", "Open Parentheses", "Close Parentheses",
-	"Open Brace", "Close Brace", "Comma", "Semicolon", "Return", "Integer Literal",
-	"Assignment", "Plus", "Negation", "Multiplication", "Division", "Logical NOT", 
-	"Bitwise Compliment", "Less Than", "Greater Than", "Less Than Or Equal To",
-	"Greater Than Or Equal To", "AND", "BitAND", "OR", "BitOR", "Equal", "Not Equal",
-	"XOR", "Bit Shift Left", "Bit Shift Right", "Modulo", "Question Mark", "Colon",
+	"ERROR", 
+    "EOF", 
+    "Keyword", 
+    "Identifier", 
+    "Open Parentheses", 
+    "Close Parentheses",
+	"Open Brace", 
+    "Close Brace", 
+    "Comma", 
+    "Semicolon", 
+    "Return", 
+    "Literal",
+    "Signed 32",
+    "Signed 64",
+    "Unsigned 32",
+    "Unsigned 64",
+    "Float 32",
+    "Float 64",
+	"Assignment", 
+    "Plus", 
+    "Negation", 
+    "Multiplication", 
+    "Division", 
+    "Logical NOT", 
+	"Bitwise Compliment", 
+    "Less Than", 
+    "Greater Than", 
+    "Less Than Or Equal To",
+	"Greater Than Or Equal To", 
+    "AND", 
+    "BitAND", 
+    "OR", 
+    "BitOR", 
+    "Equal", 
+    "Not Equal",
+	"XOR", 
+    "Bit Shift Left", 
+    "Bit Shift Right", 
+    "Modulo", 
+    "Question Mark", 
+    "Colon",
 	"if Statement", "else Statement"
 };
 
