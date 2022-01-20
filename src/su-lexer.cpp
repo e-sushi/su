01a2b2be40e547fb73500dbd518ff6ec4d8767b3
dellevelled <delle.caj@gmail.com>
2022-01-20 17:26:21 -0500

    Organize lexer tokens

--- src/su-lexer.cpp
+++ src/su-lexer.cpp
@@ -1,7 +1,7 @@
 enum LexerState_ {
 	DiscernChar,
 	ReadingString,
-
+	
 }; typedef u32 LexerState;
 
 LexerState state = DiscernChar;
@@ -10,7 +10,7 @@ b32 suLexer::lex(FILE* file, array<token>& tokens) {
 	char currChar = 0;
 	char* buffptr = 0;
 	u32 lines = 1;
-
+	
 	fseek(file, 0, SEEK_END);
 	u32 size = ftell(file);
 	buffptr = (char*)malloc(size);
@@ -19,24 +19,28 @@ b32 suLexer::lex(FILE* file, array<token>& tokens) {
 	u32 read = fread(buffptr, 1, size, file);
 	
 	buffptr[read] = 0;
-
+	
 	cstring buff{ buffptr, read };
-
+	
 	char* chunk_start = 0;
 #define chunksiz buff.str - chunk_start
 #define chunkcmp chunk_start, chunksiz
 #define chunkstr string(chunk_start, buff.str - chunk_start)
-
+	
 	while (buff.count) {
 		switch (state) {
 			case DiscernChar: {
 				switch (buff[0]) {
-					case ';': { tokens.add(token{ ";", Token_Semicolon,  lines }); advance(&buff); }break;
-					case '{': { tokens.add(token{ "{", Token_OpenBrace,  lines }); advance(&buff); }break;
-					case '}': { tokens.add(token{ "}", Token_CloseBrace, lines }); advance(&buff); }break;
-					case '(': { tokens.add(token{ "(", Token_OpenParen,  lines }); advance(&buff); }break;
-					case ')': { tokens.add(token{ ")", Token_CloseParen, lines }); advance(&buff); }break;
-					case ',': { tokens.add(token{ ",", Token_Comma,      lines }); advance(&buff); }break;
+					case ';': { tokens.add(token{ ";", Token_Semicolon,    lines }); advance(&buff); }break;
+					case '{': { tokens.add(token{ "{", Token_OpenBrace,    lines }); advance(&buff); }break;
+					case '}': { tokens.add(token{ "}", Token_CloseBrace,   lines }); advance(&buff); }break;
+					case '(': { tokens.add(token{ "(", Token_OpenParen,    lines }); advance(&buff); }break;
+					case ')': { tokens.add(token{ ")", Token_CloseParen,   lines }); advance(&buff); }break;
+					case ',': { tokens.add(token{ ",", Token_Comma,        lines }); advance(&buff); }break;
+					case '?': { tokens.add(token{ "?", Token_QuestionMark, lines }); advance(&buff); }break;
+					case ':': { tokens.add(token{ ",", Token_Colon,        lines }); advance(&buff); }break;
+					case '.': { tokens.add(token{ ".", Token_Dot,          lines }); advance(&buff); }break;
+					
 					case '+': {
 						if (buff[1] == '=') { tokens.add(token{ "+=", Token_PlusAssignment, lines }); advance(&buff, 2); }
 						else { tokens.add(token{ "+", Token_Plus, lines }); advance(&buff); }
@@ -93,8 +97,6 @@ b32 suLexer::lex(FILE* file, array<token>& tokens) {
 						else if (buff[1] == '>') { tokens.add(token{ ">>", Token_BitShiftRight, lines }); advance(&buff, 2); }
 						else { tokens.add(token{ ">", Token_GreaterThan, lines }); advance(&buff); }
 					}break;
-					case '?': { tokens.add(token{ "", Token_QuestionMark, lines }); advance(&buff); }break;
-					case ':': { tokens.add(token{ "", Token_Colon, lines }); advance(&buff); }break;
 					case '\n': { lines++; advance(&buff); }break;
 					case '\t':
 					case ' ': { advance(&buff); }break;
@@ -105,7 +107,7 @@ b32 suLexer::lex(FILE* file, array<token>& tokens) {
 					}break;
 				}
 			}break;
-				
+			
 			case ReadingString: {
 				switch (buff[0]) {
 					case ';': case ' ': case '{': case '}': case '(': case ')':
@@ -114,14 +116,15 @@ b32 suLexer::lex(FILE* file, array<token>& tokens) {
 					case '%': case ':': case '?': case '>': case '^': case '\t': {
 						if (isalpha(*chunk_start)) {
 							if      (!strncmp("return", chunk_start, 6)) tokens.add(token{ "return", Token_Return,     lines });
+							else if (!strncmp("if",     chunk_start, 2)) tokens.add(token{ "if",     Token_If,         lines });
+							else if (!strncmp("else",   chunk_start, 4)) tokens.add(token{ "else",   Token_Else,       lines });
+							else if (!strncmp("struct", chunk_start, 4)) tokens.add(token{ "struct", Token_Struct,     lines });
 							else if (!strncmp("s32",    chunk_start, 3)) tokens.add(token{ "s32",    Token_Signed32,   lines });
 							else if (!strncmp("s64",    chunk_start, 3)) tokens.add(token{ "s64",    Token_Signed64,   lines });
 							else if (!strncmp("u32",    chunk_start, 3)) tokens.add(token{ "u32",    Token_Unsigned32, lines });
 							else if (!strncmp("u64",    chunk_start, 3)) tokens.add(token{ "u64",    Token_Unsigned64, lines });
 							else if (!strncmp("f32",    chunk_start, 3)) tokens.add(token{ "f32",    Token_Float32,    lines });
 							else if (!strncmp("f64",    chunk_start, 3)) tokens.add(token{ "f64",    Token_Float64,    lines });
-							else if (!strncmp("if",     chunk_start, 2)) tokens.add(token{ "if",     Token_If,         lines });
-							else if (!strncmp("else",   chunk_start, 4)) tokens.add(token{ "else",   Token_Else,       lines });
 							else tokens.add(token{ chunkstr, Token_Identifier, lines });
 						}
 						else {
@@ -137,7 +140,7 @@ b32 suLexer::lex(FILE* file, array<token>& tokens) {
 						
 						if (buff[0] == '\n') lines++;
 						state = DiscernChar;
-
+						
 					}break;
 					default: { advance(&buff); }break;
 				}
