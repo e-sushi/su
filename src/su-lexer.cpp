7665d12b7a24a9fd444feb2e083f5ce760561789
e-sushi <officialaxos7@gmail.com>
2022-01-22 18:23:16 -0500

    make lexer discern literal types

--- src/su-lexer.cpp
+++ src/su-lexer.cpp
@@ -1,6 +1,7 @@
 enum LexerState_ {
 	DiscernChar,
 	ReadingString,
+	ReadingStringLiteral,
 }; typedef u32 LexerState;
 
 LexerState state = DiscernChar;
@@ -10,6 +11,7 @@ b32 suLexer::lex(const string& file, array<token>& tokens) {
 	cstring buff{file.str, file.count};
 	u32 lines = 1;
 	
+	b32 readingstring = 0;
 	char* chunk_start = 0;
 #define chunksiz buff.str - chunk_start
 #define chunkcmp chunk_start, chunksiz
@@ -19,75 +21,82 @@ b32 suLexer::lex(const string& file, array<token>& tokens) {
 		switch (state) {
 			case DiscernChar: {
 				switch (buff[0]) {
-					case ';': { tokens.add(token{ ";", Token_Semicolon,    lines }); advance(&buff); }break;
-					case '{': { tokens.add(token{ "{", Token_OpenBrace,    lines }); advance(&buff); }break;
-					case '}': { tokens.add(token{ "}", Token_CloseBrace,   lines }); advance(&buff); }break;
-					case '(': { tokens.add(token{ "(", Token_OpenParen,    lines }); advance(&buff); }break;
-					case ')': { tokens.add(token{ ")", Token_CloseParen,   lines }); advance(&buff); }break;
-					case ',': { tokens.add(token{ ",", Token_Comma,        lines }); advance(&buff); }break;
-					case '?': { tokens.add(token{ "?", Token_QuestionMark, lines }); advance(&buff); }break;
-					case ':': { tokens.add(token{ ",", Token_Colon,        lines }); advance(&buff); }break;
-					case '.': { tokens.add(token{ ".", Token_Dot,          lines }); advance(&buff); }break;
+					case ';': { tokens.add(token{ ";", Token_Semicolon,    Token_ControlCharacter, lines }); advance(&buff); }break;
+					case '{': { tokens.add(token{ "{", Token_OpenBrace,    Token_ControlCharacter, lines }); advance(&buff); }break;
+					case '}': { tokens.add(token{ "}", Token_CloseBrace,   Token_ControlCharacter, lines }); advance(&buff); }break;
+					case '(': { tokens.add(token{ "(", Token_OpenParen,    Token_ControlCharacter, lines }); advance(&buff); }break;
+					case ')': { tokens.add(token{ ")", Token_CloseParen,   Token_ControlCharacter, lines }); advance(&buff); }break;
+					case ',': { tokens.add(token{ ",", Token_Comma,        Token_ControlCharacter, lines }); advance(&buff); }break;
+					case '?': { tokens.add(token{ "?", Token_QuestionMark, Token_ControlCharacter, lines }); advance(&buff); }break;
+					case ':': { tokens.add(token{ ",", Token_Colon,        Token_ControlCharacter, lines }); advance(&buff); }break;
+					case '.': { tokens.add(token{ ".", Token_Dot,          Token_ControlCharacter, lines }); advance(&buff); }break;
 					
 					case '+': {
-						if (buff[1] == '=') { tokens.add(token{ "+=", Token_PlusAssignment, lines }); advance(&buff, 2); }
-						else { tokens.add(token{ "+", Token_Plus, lines }); advance(&buff); }
+						if (buff[1] == '=') { tokens.add(token{ "+=", Token_PlusAssignment, Token_Operator, lines }); advance(&buff, 2); }
+						else if (buff[1] == '+') { tokens.add(token{ "++", Token_Increment, Token_Operator, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "+", Token_Plus, Token_Operator, lines }); advance(&buff); }
 					}break;
 					case '-': {
-						if (buff[1] == '=') { tokens.add(token{ "-=", Token_NegationAssignment, lines }); advance(&buff, 2); }
-						else { tokens.add(token{ "-", Token_Negation, lines }); advance(&buff); }
+						if (buff[1] == '=') { tokens.add(token{ "-=", Token_NegationAssignment, Token_Operator, lines }); advance(&buff, 2); }
+						else if (buff[1] == '-') { tokens.add(token{ "--", Token_Decrememnt, Token_Operator, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "-", Token_Negation, Token_Operator, lines }); advance(&buff); }
 					}break;
 					case '*': {
-						if (buff[1] == '=') { tokens.add(token{ "*=", Token_MultiplicationAssignment, lines }); advance(&buff, 2); }
-						else { tokens.add(token{ "*", Token_Multiplication, lines }); advance(&buff); }
+						if (buff[1] == '=') { tokens.add(token{ "*=", Token_MultiplicationAssignment, Token_Operator, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "*", Token_Multiplication, Token_Operator, lines }); advance(&buff); }
 					}break;
 					case '/': {
-						if (buff[1] == '=') { tokens.add(token{ "/=", Token_DivisionAssignment, lines }); advance(&buff, 2); }
-						else { tokens.add(token{ "/", Token_Division, lines }); advance(&buff); }
+						if (buff[1] == '=') { tokens.add(token{ "/=", Token_DivisionAssignment, Token_Operator, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "/", Token_Division, Token_Operator, lines }); advance(&buff); }
 					}break;
 					case '~': {
-						if (buff[1] == '=') { tokens.add(token{ "~=", Token_BitNOTAssignment, lines }); advance(&buff, 2); }
-						else { tokens.add(token{ "~", Token_BitNOT, lines }); advance(&buff); }
+						if (buff[1] == '=') { tokens.add(token{ "~=", Token_BitNOTAssignment, Token_Operator, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "~", Token_BitNOT, Token_Operator, lines }); advance(&buff); }
 					}break;
 					case '&': {
-						if (buff[1] == '=') { tokens.add(token{ "&=", Token_BitANDAssignment, lines }); advance(&buff, 2); }
-						else if (buff[1] == '&') { tokens.add(token{ "&&", Token_AND, lines }); advance(&buff, 2); }
-						else { tokens.add(token{ "&", Token_BitAND, lines }); advance(&buff); }
+						if (buff[1] == '=') { tokens.add(token{ "&=", Token_BitANDAssignment, Token_Operator, lines }); advance(&buff, 2); }
+						else if (buff[1] == '&') { tokens.add(token{ "&&", Token_AND, Token_Operator, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "&", Token_BitAND, Token_Operator, lines }); advance(&buff); }
 					}break;
 					case '|': {
-						if (buff[1] == '=') { tokens.add(token{ "|=", Token_BitORAssignment, lines }); advance(&buff, 2); }
-						else if (buff[1] == '|') { tokens.add(token{ "||", Token_OR, lines }); advance(&buff, 2); }
-						else { tokens.add(token{ "|", Token_BitOR, lines }); advance(&buff); }
+						if (buff[1] == '=') { tokens.add(token{ "|=", Token_BitORAssignment, Token_Operator, lines }); advance(&buff, 2); }
+						else if (buff[1] == '|') { tokens.add(token{ "||", Token_OR, Token_Operator, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "|", Token_BitOR, Token_Operator, lines }); advance(&buff); }
 					}break;
 					case '^': {
-						if (buff[1] == '=') { tokens.add(token{ "^=", Token_BitXORAssignment, lines }); advance(&buff, 2); }
-						else { tokens.add(token{ "^", Token_BitXOR, lines }); advance(&buff); }
+						if (buff[1] == '=') { tokens.add(token{ "^=", Token_BitXORAssignment, Token_Operator, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "^", Token_BitXOR, Token_Operator, lines }); advance(&buff); }
 					}break;
 					case '%': {
-						if (buff[1] == '=') { tokens.add(token{ "%=", Token_Equal, lines }); advance(&buff, 2); }
-						else { tokens.add(token{ "%", Token_Modulo, lines }); advance(&buff); }
+						if (buff[1] == '=') { tokens.add(token{ "%=", Token_Equal, Token_Operator, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "%", Token_Modulo, Token_Operator, lines }); advance(&buff); }
 					}break;
 					case '=': {
-						if (buff[1] == '=') { tokens.add(token{ "==", Token_Equal, lines }); advance(&buff, 2); }
-						else { tokens.add(token{ "=", Token_Assignment, lines }); advance(&buff); }
+						if (buff[1] == '=') { tokens.add(token{ "==", Token_Equal, Token_Operator, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "=", Token_Assignment, Token_Operator, lines }); advance(&buff); }
 					}break;
 					case '!': {
-						if (buff[1] == '=') { tokens.add(token{ "!=", Token_NotEqual, lines }); advance(&buff, 2); }
-						else { tokens.add(token{ "!", Token_LogicalNOT, lines }); advance(&buff); }
+						if (buff[1] == '=') { tokens.add(token{ "!=", Token_NotEqual, Token_Operator, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "!", Token_LogicalNOT, Token_Operator, lines }); advance(&buff); }
 					}break;
 					case '<': {
-						if (buff[1] == '=') { tokens.add(token{ "<=", Token_LessThanOrEqual, lines }); advance(&buff, 2); }
-						else if (buff[1] == '<') { tokens.add(token{ "<<", Token_BitShiftLeft, lines }); advance(&buff, 2); }
-						else { tokens.add(token{ "<", Token_LessThan, lines }); advance(&buff); }
+						if (buff[1] == '=') { tokens.add(token{ "<=", Token_LessThanOrEqual, Token_Operator, lines }); advance(&buff, 2); }
+						else if (buff[1] == '<') { tokens.add(token{ "<<", Token_BitShiftLeft, Token_Operator, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ "<", Token_LessThan, Token_Operator, lines }); advance(&buff); }
 					}break;
 					case '>': {
-						if (buff[1] == '=') { tokens.add(token{ ">=", Token_GreaterThanOrEqual, lines }); advance(&buff, 2); }
-						else if (buff[1] == '>') { tokens.add(token{ ">>", Token_BitShiftRight, lines }); advance(&buff, 2); }
-						else { tokens.add(token{ ">", Token_GreaterThan, lines }); advance(&buff); }
+						if (buff[1] == '=') { tokens.add(token{ ">=", Token_GreaterThanOrEqual, Token_Operator, lines }); advance(&buff, 2); }
+						else if (buff[1] == '>') { tokens.add(token{ ">>", Token_BitShiftRight, Token_Operator, lines }); advance(&buff, 2); }
+						else { tokens.add(token{ ">", Token_GreaterThan, Token_Operator, lines }); advance(&buff); }
 					}break;
 					case '\n': { lines++; advance(&buff); }break;
 					case '\t':
 					case ' ': { advance(&buff); }break;
+					case '"': {
+						state = ReadingStringLiteral;
+						advance(&buff);
+						chunk_start = buff.str;
+					}break;
 					default: { //if this char is none of the above cases we start looking for a worded token
 						state = ReadingString;
 						chunk_start = buff.str;
@@ -103,39 +112,72 @@ b32 suLexer::lex(const string& file, array<token>& tokens) {
 					case '=': case '!': case '~': case '&': case '|': case '\t':
 					case '%': case ':': case '?': case '>': case '^': case '\n': {
 						if (isalpha(*chunk_start)) {
-							if      (!strncmp("return", chunk_start, 6)) tokens.add(token{ "return", Token_Return,     lines });
-							else if (!strncmp("if",     chunk_start, 2)) tokens.add(token{ "if",     Token_If,         lines });
-							else if (!strncmp("else",   chunk_start, 4)) tokens.add(token{ "else",   Token_Else,       lines });
-							else if (!strncmp("struct", chunk_start, 4)) tokens.add(token{ "struct", Token_Struct,     lines });
-							else if (!strncmp("s32",    chunk_start, 3)) tokens.add(token{ "s32",    Token_Signed32,   lines });
-							else if (!strncmp("s64",    chunk_start, 3)) tokens.add(token{ "s64",    Token_Signed64,   lines });
-							else if (!strncmp("u32",    chunk_start, 3)) tokens.add(token{ "u32",    Token_Unsigned32, lines });
-							else if (!strncmp("u64",    chunk_start, 3)) tokens.add(token{ "u64",    Token_Unsigned64, lines });
-							else if (!strncmp("f32",    chunk_start, 3)) tokens.add(token{ "f32",    Token_Float32,    lines });
-							else if (!strncmp("f64",    chunk_start, 3)) tokens.add(token{ "f64",    Token_Float64,    lines });
-							else if (!strncmp("any",    chunk_start, 3)) tokens.add(token{ "any",    Token_Any,        lines });
-							else tokens.add(token{ chunkstr, Token_Identifier, lines });
+							if      (!strncmp("return", chunk_start, 6)) tokens.add(token{ "return", Token_Return,     Token_ControlKeyword, lines });
+							else if (!strncmp("if",     chunk_start, 2)) tokens.add(token{ "if",     Token_If,         Token_ControlKeyword, lines });
+							else if (!strncmp("else",   chunk_start, 4)) tokens.add(token{ "else",   Token_Else,       Token_ControlKeyword, lines });
+							else if (!strncmp("for",    chunk_start, 3)) tokens.add(token{ "for",    Token_For,        Token_ControlKeyword, lines });
+							else if (!strncmp("while",  chunk_start, 5)) tokens.add(token{ "while",  Token_While,      Token_ControlKeyword, lines });
+							else if (!strncmp("struct", chunk_start, 4)) tokens.add(token{ "struct", Token_Struct,     Token_ControlKeyword, lines });
+							else if (!strncmp("s32",    chunk_start, 3)) tokens.add(token{ "s32",    Token_Signed32,   Token_Typename,       lines });
+							else if (!strncmp("s64",    chunk_start, 3)) tokens.add(token{ "s64",    Token_Signed64,   Token_Typename,       lines });
+							else if (!strncmp("u32",    chunk_start, 3)) tokens.add(token{ "u32",    Token_Unsigned32, Token_Typename,       lines });
+							else if (!strncmp("u64",    chunk_start, 3)) tokens.add(token{ "u64",    Token_Unsigned64, Token_Typename,       lines });
+							else if (!strncmp("f32",    chunk_start, 3)) tokens.add(token{ "f32",    Token_Float32,    Token_Typename,       lines });
+							else if (!strncmp("f64",    chunk_start, 3)) tokens.add(token{ "f64",    Token_Float64,    Token_Typename,       lines });
+							else if (!strncmp("any",    chunk_start, 3)) tokens.add(token{ "any",    Token_Any,        Token_Typename,       lines });
+							else if (!strncmp("str",    chunk_start, 3)) tokens.add(token{ "any",    Token_String,     Token_Typename,       lines });
+							else tokens.add(token{ chunkstr, Token_Identifier, Token_Identifier, lines });
 						}
 						else {
 							b32 valid = 1;
-							forI(buff.str - chunk_start) if (!isnumber(chunk_start[i])) valid = 0;
-							if (valid) tokens.add(token{ chunkstr, Token_Literal, lines });
+							b32 isfloat = 0;
+							forI(buff.str - chunk_start) { if (!isnumber(chunk_start[i])) valid = 0; if (chunk_start[i] == '.') isfloat = 1; }
+							if (valid) tokens.add(token{ chunkstr, (isfloat ? Token_LiteralFloat : Token_LiteralInteger), Token_Literal, lines });
 							else {
 								//tokens.add(token{ chunkstr, Token_ERROR, lines });
 								//PRINTLN("TOKEN ERROR ON TOKEN " << chunkstr);
 							}
 							// TODO error out of the program here
 						}
-						
+
 						if (buff[0] == '\n') lines++;
 						state = DiscernChar;
-						
 					}break;
+
+					case '.': {
+						if (isalpha(*chunk_start)) {
+							//must be member access period (i hope)
+							//TODO maybe add checks here that its not a keyword
+							tokens.add(token{ chunkstr, Token_Identifier, Token_Identifier, lines });
+							tokens.add(token{ ".", Token_Dot, Token_ControlCharacter, lines });
+							advance(&buff);
+						}
+						else {}//float case handled above
+					}break;
+
 					default: { advance(&buff); }break;
 				}
 			}break;
+
+			//TODO setup escape character stuff
+			case ReadingStringLiteral: {
+				switch (buff[0]) {
+					case '"': {
+						tokens.add(token{ chunkstr, Token_LiteralString, Token_Literal, lines });
+						state = DiscernChar;
+						advance(&buff);
+					}break;
+					case '\n': {
+						PRINTLN("unexpected \\n before \" on line " << lines);
+						return false;
+					}break;
+					default: { advance(&buff); }break;
+				}
+
+
+			}break;
 		}
 	}
-	tokens.add(token{ "", Token_EOF, lines });
+	tokens.add(token{ "", Token_EOF, Token_EOF, lines });
 	return true;
 }
--- src/su-lexer.h
+++ src/su-lexer.h
@@ -8,118 +8,135 @@
 
 
 enum Token_Type {
-    Token_ERROR,                    // when something doesnt make sense during lexing
-    Token_EOF,                      // end of file
+	Token_ERROR,                    // when something doesnt make sense during lexing
+	Token_EOF,                      // end of file
 	
-    Token_Identifier,               // function/variable names
-    Token_Literal,                  // 1, 2, 3.221, "string", 'c'
+	Token_Identifier,               // function/variable names                 
+									
+	//// literal ////
+	Token_Literal,
+	Token_LiteralFloat,               
+	Token_LiteralInteger,
+	Token_LiteralString,
 	
 	//// control characters ////
-    Token_Semicolon,                // ;
-    Token_OpenBrace,                // {
-    Token_CloseBrace,               // }
-    Token_OpenParen,                // (
-    Token_CloseParen,               // )
-    Token_Comma,                    // ,
+	Token_ControlCharacter,
+	Token_Semicolon,                // ;
+	Token_OpenBrace,                // {
+	Token_CloseBrace,               // }
+	Token_OpenParen,                // (
+	Token_CloseParen,               // )
+	Token_Comma,                    // ,
 	Token_QuestionMark,             // ?
-    Token_Colon,                    // :
-    Token_Dot,                      // .
+	Token_Colon,                    // :
+	Token_Dot,                      // .
 	
 	//// operators ////
-    Token_Plus,                     // +
-    Token_PlusAssignment,           // +=
-    Token_Negation,                 // -
-    Token_NegationAssignment,       // -=
-    Token_Multiplication,           // *
-    Token_MultiplicationAssignment, // *=
-    Token_Division,                 // /
-    Token_DivisionAssignment,       // /=
-    Token_BitNOT,                   // ~
-    Token_BitNOTAssignment,         // ~=
-    Token_BitAND,                   // &
-    Token_BitANDAssignment,         // &=
-    Token_AND,                      // &&
-    Token_BitOR,                    // |
-    Token_BitORAssignment,          // |=
-    Token_OR,                       // ||
-    Token_BitXOR,                   // ^
-    Token_BitXORAssignment,         // ^=
-    Token_BitShiftLeft,             // <<
-    Token_BitShiftRight,            // >>
-    Token_Modulo,                   // %
-    Token_ModuloAssignment,         // %=
-    Token_Assignment,               // =
-    Token_Equal,                    // ==
-    Token_LogicalNOT,               // !
-    Token_NotEqual,                 // !=
-    Token_LessThan,                 // <
-    Token_LessThanOrEqual,          // <=
-    Token_GreaterThan,              // >
-    Token_GreaterThanOrEqual,       // >=
+	Token_Operator,
+	Token_Plus,                     // +
+	Token_Increment,                // ++
+	Token_PlusAssignment,           // +=
+	Token_Negation,                 // -
+	Token_Decrememnt,               // --
+	Token_NegationAssignment,       // -=
+	Token_Multiplication,           // *
+	Token_MultiplicationAssignment, // *=
+	Token_Division,                 // /
+	Token_DivisionAssignment,       // /=
+	Token_BitNOT,                   // ~
+	Token_BitNOTAssignment,         // ~=
+	Token_BitAND,                   // &
+	Token_BitANDAssignment,         // &=
+	Token_AND,                      // &&
+	Token_BitOR,                    // |
+	Token_BitORAssignment,          // |=
+	Token_OR,                       // ||
+	Token_BitXOR,                   // ^
+	Token_BitXORAssignment,         // ^=
+	Token_BitShiftLeft,             // <<
+	Token_BitShiftRight,            // >>
+	Token_Modulo,                   // %
+	Token_ModuloAssignment,         // %=
+	Token_Assignment,               // =
+	Token_Equal,                    // ==
+	Token_LogicalNOT,               // !
+	Token_NotEqual,                 // !=
+	Token_LessThan,                 // <
+	Token_LessThanOrEqual,          // <=
+	Token_GreaterThan,              // >
+	Token_GreaterThanOrEqual,       // >=
 	
 	//// control keywords ////
+	Token_ControlKeyword,
 	Token_Return,                   // return
-    Token_If,                       // if
-    Token_Else,                     // else
+	Token_If,                       // if
+	Token_Else,                     // else
+	Token_For,                      // for
+	Token_While,                    // while        
 	Token_Struct,                   // struct
+
 	
 	//// type keywords ////
+	Token_Typename,
+	Token_Signed8,                  // s8
 	Token_Signed32,                 // s32 
-    Token_Signed64,                 // s64 
-    Token_Unsigned32,               // u32 
-    Token_Unsigned64,               // u64 
-    Token_Float32,                  // f32 
-    Token_Float64,                  // f64 
-    Token_Any,                      // any
+	Token_Signed64,                 // s64
+	Token_Unsigned8,                // u8
+	Token_Unsigned32,               // u32 
+	Token_Unsigned64,               // u64 
+	Token_Float32,                  // f32 
+	Token_Float64,                  // f64 
+	Token_String,                   // str
+	Token_Any,                      // any
 };
 
 static const char* tokenStrings[] = {
-    "ERROR",
+	"ERROR",
 	"EOF",
 	
-    "Identifier",
+	"Identifier",
 	"Literal",
 	
-    "Semicolon",
-    "OpenBrace",
-    "CloseBrace",
-    "OpenParen",
-    "CloseParen",
-    "Comma",
+	"Semicolon",
+	"OpenBrace",
+	"CloseBrace",
+	"OpenParen",
+	"CloseParen",
+	"Comma",
 	"QuestionMark",
-    "Colon",
-    "Dot",
+	"Colon",
+	"Dot",
 	
-    "Plus",
-    "PlusAssignment",
-    "Negation",
-    "NegationAssignment",
-    "Multiplication",
-    "MultiplicationAssignment",
-    "Division",
-    "DivisionAssignment",
-    "BitNOT",
-    "BitNOTAssignment",
-    "BitAND",
-    "BitANDAssignment",
-    "AND",
-    "BitOR",
-    "BitORAssignment",
-    "OR",
-    "BitXOR",
-    "BitXORAssignment",
-    "BitShiftLeft",
-    "BitShiftRight",
-    "Modulo",
-    "ModuloAssignment",
-    "Assignment",
-    "Equal",
-    "LogicalNOT",
-    "NotEqual",
-    "LessThan",
-    "LessThanOrEqual",
-    "GreaterThan",
-    "GreaterThanOrEqual",
+	"Plus",
+	"PlusAssignment",
+	"Negation",
+	"NegationAssignment",
+	"Multiplication",
+	"MultiplicationAssignment",
+	"Division",
+	"DivisionAssignment",
+	"BitNOT",
+	"BitNOTAssignment",
+	"BitAND",
+	"BitANDAssignment",
+	"AND",
+	"BitOR",
+	"BitORAssignment",
+	"OR",
+	"BitXOR",
+	"BitXORAssignment",
+	"BitShiftLeft",
+	"BitShiftRight",
+	"Modulo",
+	"ModuloAssignment",
+	"Assignment",
+	"Equal",
+	"LogicalNOT",
+	"NotEqual",
+	"LessThan",
+	"LessThanOrEqual",
+	"GreaterThan",
+	"GreaterThanOrEqual",
 	
 	"Return",
 	"If",
@@ -143,6 +160,7 @@ static const char* tokenStrings[] = {
 struct token {
 	string str;
 	Token_Type type;
+	Token_Type group;
 	u64 line = 0;
 };
 
