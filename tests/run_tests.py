3b43827cf6b52fc383e1209f0582c3acee049a0a
dellevelled <delle.caj@gmail.com>
2022-01-22 18:58:41 -0500

    Upgrade the test script

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,10 +1,18 @@
 #run_tests.py
-#expected to be run from su/tests
-#TODO add command-line args for su.exe path and tests filtering
+#this file is expected to be in su/tests, but can be run from anywhere
+#_______________________
+#Command Line Arguments:
+#-e [str]
+#  executable path
+#  default: tests_dir + "..\\build\\debug\\su.exe"
+#
+#-f [str]
+#  test filter
+#  default: "*"
+#_______________________
+#TODOs:
 
-import os,subprocess,enum,ctypes
-
-su_exe_path = "..\\build\\debug\\su.exe"
+import sys,os,subprocess,enum,ctypes
 
 ReturnCode_Success                = 0
 ReturnCode_No_File_Passed         = 1
@@ -92,67 +100,122 @@ tests = [
 ];
 
 def main():
+    #gather command line args
+    tests_dir = os.path.dirname(__file__)
+    su_exe_path = os.path.join(tests_dir, "..\\build\\debug\\su.exe")
+    test_filter = "*"
+
+    arg_index = 1;
+    for _ in range(len(sys.argv)):
+        if(_ != arg_index): continue;
+        #print(arg_index)
+        #print(sys.argv[arg_index])
+        
+        if(sys.argv[arg_index] == "-e"):
+            if(arg_index+1 >= len(sys.argv)):
+                print("ERROR: no argument passed to -e");
+                return;
+            elif(sys.argv[arg_index+1].startswith("-")):
+                print("ERROR: invalid argument for -e:", sys.argv[arg_index+1]);
+                return;
+            else:
+                su_exe_path = sys.argv[arg_index+1];
+                arg_index += 1;
+        elif(sys.argv[arg_index] == "-f"):
+            if(arg_index+1 >= len(sys.argv)):
+                print("ERROR: no argument passed to -f");
+                return;
+            elif(sys.argv[arg_index+1].startswith("-")):
+                print("ERROR: invalid argument for -f:", sys.argv[arg_index+1]);
+                return;
+            else:
+                test_filter = sys.argv[arg_index+1];
+                arg_index += 1;
+        else:
+            print("ERROR: unknown flag: ", sys.argv[arg_index]);
+            return;
+        arg_index += 1;
+    #print(su_exe_path);
+    #print(test_filter);
+    
+    filter_wildcard = test_filter.endswith("*")
+
+    #iterate thru tests
     tests_total  = 0;
     tests_passed = 0;
     tests_failed = 0;
-
     for path,expected in tests:
+        #print(path);
+        #print(path.startswith(test_filter[0:-1]))
+        
+        if(filter_wildcard):
+            if not(path.startswith(test_filter[0:-1])):
+                #print(1);
+                continue;
+        elif(path != test_filter):
+                #print(2);
+                continue;
+    
         tests_total += 1;
-        path_arr = path.split('/');
-        group = path_arr[0];
-        type  = path_arr[1];
-        name  = path_arr[2];
-        file     = path[:-3];
+        full_path = tests_dir + "/" + path;
+        path_arr = full_path.split('/');
+        group = path_arr[-3];
+        type  = path_arr[-2];
+        name  = path_arr[-1];
+        file     = full_path[:-3];
         file_s   = file+".s";
         file_su  = file+".su";
         file_exe = file+".exe";
-        compile_cmd = su_exe_path+" -i "+file_su+" -o "+"/".join([group,type,""]);
+        compile_cmd = su_exe_path+" -i "+file_su+" -o "+"/".join([tests_dir,group,type,""]);
         #print(compile_cmd)
         
         if(type == 'valid'): #valid tests should return errorlevel 0 from su.exe
             try:
                 subprocess.run(compile_cmd, capture_output=True, check=True);
                 subprocess.run("gcc "+"-m64 "+file_s+" -o "+file_exe);
-                if not(os.path.exists(file_exe)): continue;
+                if not(os.path.exists(file_exe)):
+                    print("%s %s (link error)" % (path.ljust(60, '_'), "FAILED"));
+                    tests_failed += 1;
+                    continue;
                 
                 try:
                     subprocess.run(file_exe, capture_output=True, check=True);
-                    #print("%-60s%s (E: %d; A: %d)" % (file, "PASSED", expected, 0));
-                    print("%-60s%s" % (file, "PASSED"));
+                    #print("%-60s %s (E: %d; A: %d)" % (path, "PASSED", expected, 0));
+                    print("%-60s %s" % (path, "PASSED"));
                     tests_passed += 1;
                 except subprocess.CalledProcessError as err:
                     actual = ctypes.c_int32(err.returncode).value;
                     if((actual == expected) or (expected == "true" and actual != 0)):
-                        #print("%-60s%s (E: %d; A: %d)" % (file, "PASSED", expected, actual));
-                        print("%-60s%s" % (file, "PASSED"));
+                        #print("%-60s%s (E: %d; A: %d)" % (path, "PASSED", expected, actual));
+                        print("%-60s %s" % (path, "PASSED"));
                         tests_passed += 1;
                     else:
-                        print("%-60s%s (E: %d; A: %d)" % (file, "FAILED", expected, actual));
+                        print("%s %s (E: %d; A: %d)" % (path.ljust(60, '_'), "FAILED", expected, actual));
                         tests_failed += 1;
                 os.remove(file_exe);
             except subprocess.CalledProcessError as err:
                 actual = ctypes.c_int32(err.returncode).value;
-                print("%-60s%s (compile error: %d)" % (file, "FAILED", actual));
+                print("%s %s (compile error: %d)" % (path.ljust(60, '_'), "FAILED", actual));
                 tests_failed += 1;
         elif(type == 'invalid'):
             try:
                 subprocess.run(compile_cmd, capture_output=True, check=True);
                 
                 os.remove(file_s);
-                print("%-60s%s (no compile error)" % (file, "FAILED"));
+                print("%s %s (no compile error)" % (path.ljust(60, '_'), "FAILED"));
                 tests_failed += 1;
             except subprocess.CalledProcessError as err:
                 actual = ctypes.c_int32(err.returncode).value;
                 if((actual == expected) or (expected == "true" and actual != 0)):
-                    #print("%-60s%s (E: %d; A: %d)" % (file, "PASSED", expected, actual));
-                    print("%-60s%s" % (file, "PASSED"));
+                    #print("%-60s%s (E: %d; A: %d)" % (path, "PASSED", expected, actual));
+                    print("%-60s %s" % (path, "PASSED"));
                     tests_passed += 1;
                 else:
-                    print("%-60s%s (E: %d; A: %d)" % (file, "FAILED", expected, actual));
+                    print("%s %s (E: %d; A: %d)" % (path.ljust(60, '_'), "FAILED", expected, actual));
                     tests_failed += 1;
         else:
-            print("ERROR: malformed test path: ", path);
+            print("ERROR: test path did not match format 'group/validity/name.su': ", path);
     print("tests: %d; passed: %d; failed: %d;" % (tests_total, tests_passed, tests_failed));
 
 if __name__ == "__main__":
-    main()
+    main();
