fdae5e5ae2740193b17320dbcaa11e105224ea7c
sushi <officialaxos7@gmail.com>
2023-06-30 14:48:46 -0400

    [storage] fix stuff in Pool

---
+++ src/storage/Pool.cpp
@@ -0,0 +1,116 @@
+
+namespace amu {
+namespace pool {
+
+namespace internal {
+    
+template<typename T> global void
+new_chunk(Pool<T>& pool) {
+    const upt blocksize = sizeof(LNode) + sizeof(T);
+
+    // allocate and insert the new chunk
+    node::insert_before(&pool->chunk_root, 
+        (LNode*)util::allocate(sizeof(LNode)+pool->items_per_chunk*blocksize));
+
+    // append the first free block to our free blocks list
+    // node::insert_before(&pool->free_blocks, pool->chunk_root.prev + sizeof(LNode));
+
+    // connect the remaining free blocks
+    u8* blockstart = (u8*)(pool->chunk_root.prev + 1);
+    forI(pool->items_per_chunk) {
+        node::insert_before(&pool->free_blocks, (LNode*)(blockstart + i * blocksize));
+    }
+} // new_chunk
+
+} // namespace internal
+
+template<typename T> Pool<T>
+init(spt n_per_chunk) {
+    Pool<T> out;
+    out.lock = mutex_init();
+    out.items_per_chunk = n_per_chunk;
+
+    node::init(&out.free_blocks);
+    node::init(&out.chunk_root);
+    node::init(&out.items);
+
+    internal::new_chunk(&out);
+  
+    return out;
+} // init
+
+template<typename T> void
+deinit(Pool<T>& pool) {
+    shared_mutex_deinit(&pool->lock);
+    
+    for(LNode* n = pool->chunk_root->next; n != &pool->chunk_root; n = n->next) {
+        util::free(n);
+    }
+}
+
+template<typename T> T*
+add(Pool<T>& pool) {
+    mutex_lock(&pool->lock);
+    defer{mutex_unlock(&pool->lock);};
+
+    if(pool->free_blocks.next == &pool->free_blocks) {
+        internal::new_chunk(pool);
+    }
+
+    LNode* place = pool->free_blocks.next;
+
+    T* out = (T*)(place + 1);
+    node::remove(place);
+    node::insert_before(&pool->items, place);
+    return out;
+}
+
+template<typename T> void
+add(Pool<T>& pool, T& val) {
+    mutex_lock(&pool->lock);
+    defer{mutex_unlock(&pool->lock);};
+
+    T* place = add(pool);
+    *place = val;
+}
+
+template<typename T> void
+remove(Pool<T>& pool, T* ptr) {
+    mutex_lock(&pool->lock);
+    defer{mutex_unlock(&pool->lock);};
+
+    LNode* header = (LNode*)((u8*)ptr - sizeof(LNode));
+    node::remove(header);
+    node::insert_before(&pool->free_blocks, header);
+}
+
+template<typename T> Iterator<T>
+iterator(Pool<T>& pool) {
+    Iterator<T> out;
+    out.pool = &pool;
+    out.current = pool->items.next;
+    return out;
+}
+
+template<typename T> T*
+next(Iterator<T>* iter) {
+    mutex_lock(&iter->pool->lock);
+    if(iter->current == &iter->pool->items) return 0;
+    T* out = (T*)(iter->current + 1);
+    iter->current = iter->current->next;
+    mutex_unlock(&iter->pool->lock);
+    return out;
+}
+
+template<typename T> T*
+prev(Iterator<T>* iter) {
+    mutex_lock(&iter->pool->lock);
+    if(iter->current == &iter->pool->items) return 0;
+    T* out = (T*)(iter->current + 1);
+    iter->current = iter->current->prev;
+    mutex_unlock(&iter->pool->lock);
+    return out;
+}
+
+} // namespace pool
+} // namespace amu
---
+++ src/storage/Pool.h
@@ -0,0 +1,95 @@
+/*
+
+    A thread safe chunked array in which pointers are guaranteed to never move.
+
+    A pool consists of linked chunks which store some fixed amount of items. These
+    chunks are prefixed by a header (just an LNode), and every item in the chunk
+    is also prefixed by a header (LNode). 
+
+    This only cares about being thread safe on writing, if you want to iterate the 
+    data use amu::pool::iterator(), which gives an iterator that locks the pool
+    when it is iterating it. But keep in mind that it is only locked when calling
+    next or prev, the pool can still change between those calls.
+
+    Beyond this, you can manually lock the entire structure if you need to.
+
+    TODOs
+        We can puts stats on this structure and guard it behind BUILD_DEBUG to make
+        debugging a little easier
+
+*/
+
+#ifndef AMU_POOL_H
+#define AMU_POOL_H
+
+#include "kigu/common.h"
+#include "kigu/node.h"
+#include "kigu/array.h"
+#include "basic/Node.h"
+
+namespace amu {
+
+template<typename T>
+struct Pool {
+    mutex lock;
+
+    // how many of 'T' we store per chunk
+    u32 items_per_chunk; 
+    // root of free blocks
+    LNode free_blocks;
+    // root of chunks
+    LNode chunk_root;
+    // root of allocated items
+    // this isn't guaranteed to be in any order
+    // the order likely depends on the sequence of additions and removals of items
+    LNode items; 
+};
+
+namespace pool {
+
+// initialize a pool that will allocate chunks to store 
+// 'n_per_chunk' items of type 'T'
+template<typename T> Pool<T>
+init(spt n_per_chunk);
+
+// deinitializes a pool
+template<typename T> void
+deinit(Pool<T>& pool);
+
+// adds an item to the pool and returns a pointer to it
+// allocates a new chunk if necessary
+template<typename T> T*
+add(Pool<T>& pool);
+
+template<typename T> void
+add(Pool<T>& pool, T& val);
+
+template<typename T> void
+remove(Pool<T>& pool, T* ptr);
+
+template<typename T>
+struct Iterator {
+    Pool<T>* pool;
+    LNode* current;
+};
+
+// returns an Iterator representing the given Pool
+template<typename T> Iterator<T>
+iterator(Pool<T>& pool);
+
+// returns the current item of the Iterator
+// and then increments it
+// if we are at the end of the iteration, 0 is returned
+template<typename T>  T*
+next(Iterator<T>* iter);
+
+// returns the current item of the Iterator
+// and the decrements it
+// if we are at the beginning of the iteration, 0 is returned
+template<typename T> T*
+prev(Iterator<T>* iter);
+
+} // namespace pool
+} // namespace amu
+
+#endif // AMU_POOL_H
