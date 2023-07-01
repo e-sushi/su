d224887042bdde80a0c46097e2d447f33f319211
sushi <officialaxos7@gmail.com>
2023-06-30 23:46:32 -0400

    [gdb] add a couple printers

--- misc/amugdb.py
+++ misc/amugdb.py
@@ -20,4 +20,72 @@ class print_lnode_chain(gdb.Command):
         except Exception as e:
             print(f"error: ", e)
 print_lnode_chain()
+
+class String_printer:
+    def __init__(self, val):
+        self.val = val
+    
+    def to_string(self):
+        try:
+            val = self.val
+            return f"{val['s']}"
+        except Exception as e:
+            print(f"error: {e}")
+pp.add_printer("String", r"^amu::String$", String_printer)
+
+class DString_printer:
+    def __init__(self, val):
+        self.val = val
+    
+    def to_string(self):
+        try:
+            val = self.val
+            return f"{val['s']}"
+        except Exception as e:
+            print(f"error: {e}")
+pp.add_printer("DString", r"^amu::DString$", String_printer)
+
+class Array_printer:
+    def __init__(self, val): 
+        self.val = val
+    
+    def display_hint(self):
+        return 'array'
+
+    def to_string(self):
+        try:
+            val = self.val
+            if not val['count']:
+                return "{empty}"
+            type = str(val.type)
+            subtype = type[type.find("<")+1:type.rfind(">")]
+            return gdb.parse_and_eval(f"*(({subtype}*){val['data']})@{val['count']}")
+        except Exception as e:
+            print(f"error: {e}")
+pp.add_printer("Array", r"^amu::Array<.*>$", Array_printer)
+
+class MessagePart_printer:
+    def __init__(self, val): 
+        self.val = val
+
+    def to_string(self):
+        try:
+            val:gdb.Value = self.val
+            match str(val['type']):
+                case "amu::MessagePart::Plain": 
+                    return f"plain: {val['plain']}"
+                case "amu::MessagePart::Path": 
+                    return f"path: {val['plain']}"
+                case "amu::MessagePart::Source": print("source")
+                case "amu::MessagePart::Entity": print("entity")
+                case "amu::MessagePart::Label": print("label")
+                case "amu::MessagePart::Code": print("code")
+        except Exception as e:
+            print(f"error: {e}")
+pp.add_printer("MessagePart", r"^amu::MessagePart$", MessagePart_printer)
+
+gdb.printing.register_pretty_printer(gdb.current_objfile(), pp)
+
+
+        
     
