7bef64defe0a288a506a31069f1f44da794aae22
sushi <officialaxos7@gmail.com>
2023-07-03 11:12:45 -0400

    [messenger] more work on messenger interface and internals

--- src/Messenger.cpp
+++ src/Messenger.cpp
@@ -3,13 +3,43 @@ namespace messenger { // ---------------------------------------------------- me
 
 namespace internal {
 
-MessageFormatting* current_formatting;
+Destination* current_dest;
+
+// wraps a DString in ANSI terminal color
+// TODO(sushi) need to setup using the 8 original colors so that terminal themes work properly
+void
+wrap_color(DString& current, u32 color) {
+    DString temp = dstring::init("\e[", color, "m");
+    dstring::prepend(current, temp);
+    dstring::append(current, "\e[0m");
+    dstring::deinit(temp);
+}
 
 void
 process_part(DString& current, MessagePart& part) {
+    MessageFormatting& formatting = shared_array::readref(instance.formatting_stack, -1);
     switch(part.type) {
         case MessagePart::Plain: {
-            dstring::append(current, part.plain);
+            DString temp = dstring::init(part.plain);
+            if(current_dest->allow_color && formatting.plain.col) {
+                wrap_color(temp, (part.col? part.col : formatting.plain.col));
+            }
+            dstring::prepend(temp, formatting.plain.prefix);
+            dstring::append(temp, formatting.plain.suffix);
+            dstring::append(current, temp);
+            dstring::deinit(temp);
+        } break;
+        case MessagePart::Path: {
+            DString temp = dstring::init(part.plain);
+            // TODO(sushi) implement fancy path formatting
+            if(current_dest->allow_color) {
+                wrap_color(temp, (part.col? part.col : formatting.path.col.directory));
+            }
+            dstring::prepend(temp, formatting.path.prefix);
+            dstring::append(temp, formatting.path.suffix);
+            dstring::append(current, temp);
+            dstring::deinit(temp);
+
         } break;
         case MessagePart::Variable: {
             NotImplemented;
@@ -33,11 +63,68 @@ process_part(DString& current, MessagePart& part) {
 }
 
 // processes a given Message into a str8
+// TODO(sushi) decide if the color of message type prefixes will be customizable and if not
+//             store a static string of them wrapped in their colors
 void
 process_message(DString& current, Message& m) {
+    // dont do anything if current verbosity is less than the message's
+    if(m.type == Message::Debug && m.verbosity > compiler::instance.options.verbosity) return; 
+
+    switch(m.sender.type) {
+        case MessageSender::Compiler: {
+            static constexpr String compiler_prefix = "\e[36mcompiler\e[0m: ";
+            dstring::append(current, compiler_prefix);
+        } break;
+        case MessageSender::Source: {
+            DString temp = dstring::init(String(m.sender.source->file->name));
+            wrap_color(temp, message::color_cyan);
+            dstring::append(current, temp);
+            dstring::append(current, ": ");
+            dstring::deinit(temp);
+        } break;
+    }
+
+    switch(m.type) {
+        case Message::Normal: { // dont need to do anything special
+        } break;
+
+        case Message::Debug: { 
+            DString temp = dstring::init("debug");
+            wrap_color(temp, message::color_green);
+            dstring::append(current, temp);
+            dstring::append(current, ": ");
+            dstring::deinit(temp);
+        } break;
+
+        case Message::Warning: {
+            DString temp = dstring::init("warning");
+            wrap_color(temp, message::color_yellow);
+            dstring::append(current, temp);
+            dstring::append(current, ": ");
+            dstring::deinit(temp);
+        } break;
+
+        case Message::Error: {
+            DString temp = dstring::init("error");
+            wrap_color(temp, message::color_red);
+            dstring::append(current, temp);
+            dstring::append(current, ": ");
+            dstring::deinit(temp);
+        } break;
+
+        case Message::Note: {
+            DString temp = dstring::init("note");
+            wrap_color(temp, message::color_magenta);
+            dstring::append(current, temp);
+            dstring::append(current, ": ");
+            dstring::deinit(temp);
+        } break;
+    }
+
     forI(m.parts.count) {
         process_part(current, array::readref(m.parts, i));
     }
+
     dstring::append(current, "\n");
 }
 
@@ -47,25 +134,16 @@ Messenger instance;
 
 void 
 init() {
-    instance.messages = array::init<Message>();
+    instance.messages = shared_array::init<Message>();
+    instance.formatting_stack = shared_array::init<MessageFormatting>();
     instance.delivering = mutex_init();
 
-    MessageFormatting def = {
-        .allow_color = true,
-        .warning = {
-            .default_color = Color_Yellow,
-            .prefix = "warning:"
-        }
-        .error = {
-            
-        }
-    }
-
+    push_formatting(MessageFormatting());
 }
 
 void
 dispatch(Message message) {
-    array::push(instance.messages, message);
+    shared_array::push(instance.messages, message);
 }
 
 // dispatch a plain str8
@@ -81,35 +159,42 @@ dispatch(String message, Source* source) {
 
 void 
 deliver(Destination destination, b32 clear_messages) {
-    array::lock(instance.messages);
+    Array<Message> temp = shared_array::lock(instance.messages);
     mutex_lock(&instance.delivering);
+    defer{
+        mutex_unlock(&instance.delivering);
+        shared_array::unlock(instance.messages, temp);
+    };
 
     DString out = dstring::init();
 
-    forI(instance.messages.count) {
+    internal::current_dest = &destination;
+
+    forI(temp.count) {
         internal::process_message(out, 
-            array::readref(instance.messages, i));
+            array::readref(temp, i));
     }
 
     switch(destination.type) {
         case Destination::STLFILE: {
             size_t bytes_written = fwrite(out.s.str, 1, out.s.count, destination.stl_file);
-            printf("%zu\n", bytes_written);
         } break;
         case Destination::DESHFILE: {
             file_append(destination.desh_file, out.s.str, out.s.count);
         } break;
     }
+
+
 }
 
 void
 push_formatting(MessageFormatting formatting) {
-    array::push(instance.formatting_stack);
+    shared_array::push(instance.formatting_stack, formatting);
 }
 
 void 
 pop_formatting() {
-    array::pop(instance.formatting_stack);
+    shared_array::pop(instance.formatting_stack);
 }
 
 } // namespace messenger
@@ -117,17 +202,16 @@ pop_formatting() {
 namespace message { // ---------------------------------------------------- message
 
 Message
-init(Source* source) {
+init() {
     Message out;
     out.time = time(0);
-    out.source = source;
     out.parts = array::init<MessagePart>();
     return out;
 }
 
 template<typename... T> Message
-init(Source* source, T... args) {
-    Message out = init(source);
+init(T... args) {
+    Message out = init();
     MessagePart arr[sizeof...(T)] = {args...};
     forI(sizeof...(T)) {
         push(out, arr[i]);
@@ -155,7 +239,7 @@ prefix(Message& m, T... args) {
 }
 
 MessagePart
-plain(String s, color c) {
+plain(String s, u32 c) {
     MessagePart out;
     out.type = MessagePart::Plain;
     out.col = c;
@@ -164,7 +248,7 @@ plain(String s, color c) {
 }
 
 MessagePart
-path(String s, color c) {
+path(String s, u32 c) {
     MessagePart out;
     out.type = MessagePart::Path;
     out.col = c;
--- src/Messenger.h
+++ src/Messenger.h
@@ -16,16 +16,52 @@
 
 namespace amu {
 
-// a Message stores an Array of Parts, which allows the Messenger to
+namespace message{
+enum : u32{
+    color_none =     0,
+    color_black =    30,
+    color_red =      31,
+    color_green =    32,
+    color_yellow =   33,
+    color_blue =     34,
+    color_magenta =  35,
+    color_cyan =     36,
+    color_white =    37,
+    color_extended = 38,
+    color_def      =  39,
+    color_bright_black =   90,
+    color_bright_red =     91,
+    color_bright_green =   92,
+    color_bright_yellow =  93,
+    color_bright_blue =    94,
+    color_bright_magenta = 95,
+    color_bright_cyan =    96,
+    color_bright_white =   97,
+};
+
+namespace verbosity{
+enum : u32 {
+    always,
+    filenames,
+    stages,
+    stageparts,
+    detailed,
+    debug,
+};
+}
+} // namespace message
+
+// a Message stores an SharedArray of Parts, which allows the Messenger to
 // process a Message and format it according to current formatting 
 // settings or where it is delivering the message to
 struct MessagePart {
     enum Type : u32 {
-        Plain, Sender, Path, Source, Variable, Structure, Function, Module, Label, Code,
+        Plain, Token, Sender, Path, Source, Variable, Structure, Function, Module, Label, Code, COUNT
     };
     Type type;
     union {
         String plain; // a plain String or path
+        amu::Token token; // a token, likely representing a source location
         Entity::Variable variable;
         Entity::Structure structure;
         Entity::Function function;
@@ -34,9 +70,9 @@ struct MessagePart {
         amu::Code code; // code region to print
         amu::Source* source; // a source file whose name we will likely print
     };
-    // if this is Color_None standard colors will be applied based on type
-    // otherwise this overrides the color used
-    color col;
+    // if this is 0, default colors will be applied in processing
+    // this is set to a number from message::color_
+    u32 col;
 
     MessagePart() {}
     MessagePart(Entity::Variable v) : variable(v) {type = Variable;}
@@ -48,48 +84,83 @@ struct MessagePart {
     MessagePart(amu::Source* s) : source(s) {type = Source;}
 };
 
+// indicates who sent the message
+struct MessageSender {
+    enum Type {
+        Compiler, // the compiler when there is no specific source that 
+        Source, // some source file, no location
+        SourceLoc, // some source file with location
+    };
+    Type type;
+    amu::Source* source;
+    u32 line, column; // if the sender is Source, give us the line and column 
+
+    MessageSender() : type(Compiler) {}
+    MessageSender(Type type) : type(type) {}
+    MessageSender(amu::Source* s) : type(Source), source(s) {}
+    MessageSender(amu::Source* s, Token& t) : type(SourceLoc), source(s) {line = t.l0; column = t.c0;}
+};
+
 // a representation of a single message to be delivered to some destination
 // consists of MessageParts that the Messenger formats before delivering
 struct Message {
     // the time this message was created
     f32 time;
-    // the Source this message originates from, if any
-    Source* source; 
+
+    MessageSender sender;
 
     enum Type {
         Normal,
         Warning,
         Error,
         Note,
+        Debug,
     };
     Type type;
+
+    u32 verbosity; // set with message::verbosity
 
     Array<MessagePart> parts;
 };
 
+// this struct defines the formatting of each different MessagePart
+// its default values should always be the ones that result in the fastest processing of messages
+// anything that slows it down should be set by the user
+// if this winds up being too slow, then just get rid of custom formatting and hardcode 
+// the default behavoir
 struct MessageFormatting {
-    b32 allow_color;
-    
     // how to format message parts
     // default_color is the color applied to the entire part AND its prefix/suffix
     // prefix and suffix are strings applied before and after the part
     struct {
-        color default_color = Color_White;
+        u32 col = message::color_white;
         String prefix = "", suffix = "";
-    }
-    plain, path, sender, 
-    source, variable, structure, function, module, label, code;
+    } plain;
+
+    struct {
+        struct {
+            u32 directory = message::color_white;
+            u32 slash = message::color_white;
+            u32 file = message::color_cyan;
+        } col;
+        // when a path is very long, shorten it by replacing the interior with ...
+        // for example
+        // some/stupidly/long/path/to/the/naughty/file
+        // some/stupidly/.../naughty/file
+        b32 shorten_long_paths = false;
+        u32 long_path_min_len = 60;
+        b32 always_absolute = false;
+        b32 always_forward_slash = true;
+        String prefix = "'", suffix = "'";
+    } path;
 
 };
 
+
+
 struct Messenger {
-    Array<Message> messages; 
-
-    struct {
-
-    } options;
-
-    Array<MessageFormatting> formatting_stack;
+    SharedArray<Message> messages; 
+    SharedArray<MessageFormatting> formatting_stack;
 
     mutex delivering; // locked when the messenger is in the process of delivering messages
 };
@@ -114,8 +185,6 @@ struct Destination {
     Destination(FILE* file, b32 allow_color = true) : stl_file(file), type(STLFILE) {}
 };
 
-
-
 namespace messenger {
 
 // global messenger instance, created in Messenger.cpp
@@ -132,6 +201,7 @@ dispatch(Message message);
 // dispatch a plain str8
 void
 dispatch(String message, Source* source = 0);
+
 
 // deliver all available messages to the given destination
 // optionally clearing messages
@@ -152,12 +222,20 @@ namespace message {
 
 // initializes a Message
 Message
-init(Source* source = 0);
+init();
 
 // variadic way to initialize a Message
 // Message::Parts may be passed
 template<typename... T> Message
-init(Source* source, T... args);
+init(T... args);
+
+// initialize a debug message
+Message
+debug(u32 verbosity);
+
+// initialize a debug message with variadic arguments
+template<typename... T> Message
+debug(u32 verbosity, T... args);
 
 // pushes a Message::Part into the Message
 void
@@ -173,15 +251,15 @@ warning(Message& m);
 
 // constructs a plain part
 MessagePart
-plain(String s, color c = Color_NONE);
+plain(String s, u32 c = message::color_none);
 
 // constructs a sender
 MessagePart
-sender(String s, color c = Color_NONE);
+sender(String s, u32 c = message::color_none);
 
 // constructs a path part
 MessagePart
-path(String s, color c = Color_NONE);
+path(String s, u32 c = message::color_none);
 
 // the following helpers are unecessary to call unless you want to override the
 // color used to format them, otherwise you can just pass the object directly
@@ -201,6 +279,9 @@ path(String s, color c = Color_NONE);
 
 // MessagePart
 // label(Label* l, color c = Color_NONE)
+
+
+
 
 } // namespace message
 } // namespace amu
